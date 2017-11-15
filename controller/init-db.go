62b083db080e2e6128642c3e00e52d303cb08636
Alexander Gunkel <alexandergunkel@gmail.com>
2017-11-15 19:17:17 +0100

    Use config file for db-config

--- controller/init-db.go
+++ controller/init-db.go
@@ -4,18 +4,42 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/alexgunkel/library/models"
+	"gopkg.in/gcfg.v1"
 )
+
+type Config struct {
+	Database struct {
+		Username string
+		Password string
+		Database string
+	}
+}
 
 var db *gorm.DB
 
 func init() {
+	db = getDatabase()
+	db.AutoMigrate(&models.Book{})
+	db.AutoMigrate(&models.Author{})
+}
+
+func getDatabase() *gorm.DB {
 	var err error
-	db, err = gorm.Open("mysql", "root:very-secure@/database?charset=utf8&parseTime=True&loc=Local")
+	var args string
+	var cfg Config
+
+	err = gcfg.ReadFileInto(&cfg, "config.cfg")
+	if nil != err {
+		panic(err)
+	}
+
+	args = cfg.Database.Username + ":" + cfg.Database.Password + "@/" + cfg.Database.Database + "?charset=utf8&parseTime=True&loc=Local"
+
+	db, err = gorm.Open("mysql", args)
 
 	if err != nil {
 		panic(err)
 	}
 
-	db.AutoMigrate(&models.Book{})
-	db.AutoMigrate(&models.Author{})
+	return db
 }
